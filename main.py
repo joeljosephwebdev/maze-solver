742df567b3fb0ec9062774ef15728daa5cdacce4
joeljosephwebdev <joelrajoseph@outlook.com>
2024-12-03 18:49:45 -0800

    added a Cell class with a draw method to render each cell of the maze and a draw_move method to illustrate lines from one cell to another.

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-from graphics import Window, Point, Line
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-
-    start_point = Point(100, 100)
-    end_point = Point(100, 200)
-    line = Line(start_point, end_point)
-    win.draw_line(line, )
-
-    start_point = Point(100, 200)
-    end_point = Point(200, 200)
-    line = Line(start_point, end_point)
-    win.draw_line(line, "red")
-
-    start_point = Point(200, 200)
-    end_point = Point(200, 100)
-    line = Line(start_point, end_point)
-    win.draw_line(line, "blue")
-
-    start_point = Point(200, 100)
-    end_point = Point(100, 100)
-    line = Line(start_point, end_point)
-    win.draw_line(line, "green")
     
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
     win.wait_for_close()
 
 if __name__ == "__main__":
